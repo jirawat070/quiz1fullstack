12a11741d2ac0103ce7f3a932428fa785272f5e1
Jirawat Paenkaew <jirawat_pao@hotmail.co.th>
2018-09-17 18:19:11 +0700

    edit index,footer,navbar inheader

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ var mysql = require('mysql');
 //app.use(express.static ('static') );
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-    res.render('pages/index');
-        
-    });
+
 
 var connection = mysql.createConnection({
   host     : 'www.db4free.net',
@@ -15,34 +12,41 @@ var connection = mysql.createConnection({
   password : 'abc123**',
   database : 'db140390'
 });
+ connection.connect()
 
-connection.connect()
+app.get('/', function(req, res) {
+    res.render('pages/index');
+        
+    });
 
 app.get('/students', function(req, res) {
-    
+   
     connection.query('SELECT * from students', function (err, rows, fields) {
         if (err) throw err
-        res.render('pages/students',{students: rows})
+        
       
         console.log( rows)
+        res.render('pages/students',{students: rows})
       })
-      
-      connection.end();
-      
+    
+     
+     
 });
 
 app.get('/subjects', function(req, res) {
-    
+   
     connection.query('SELECT * from subjects', function (err, rows, fields) {
         if (err) throw err
         res.render('pages/subjects',{subjects: rows})
       
         console.log( rows)
+        res.render('pages/subjects',{subjects: rows})
       })
       
-      connection.end();
-      
+     
+        
 });
+
 
 
 
