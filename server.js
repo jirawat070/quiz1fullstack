7d3831fcf16bc8781d7e4f06f1fc2f3f14a65629
Jirawat Paenkaew <jirawat_pao@hotmail.co.th>
2018-09-17 16:29:25 +0700

    creat jumbotron

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var express = require('express');
 var mysql = require('mysql')
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -15,3 +16,58 @@ connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
 })
 
 connection.end()
+//app.use(express.static ('static') );
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+        
+    });
+
+    app.get('/about', function(req, res) {
+        var name = ['BUBBLE'];
+        var hobbies = ['Music','Movie','Programming'];
+        var bdate ='27/03/1997';
+        res.render('pages/about',{fullname : name, hobbies:hobbies,bdate: bdate});
+            
+        });
+    //Display all products
+        app.get('/products', function(req, res) {
+            var id = req.param('id');
+            var sql='select* from products';
+                if(id){
+                    sql += ' where id ='+id;
+                }
+           db.any(sql)
+            .then(function(data){
+                console.log('DATA:'+data);
+                res.render('pages/products',{products: data})
+                
+            })
+            .catch(function(error){
+                console.log('ERROR:'+error);
+            })
+
+        });
+   //Display all user
+            app.get('/users/:id', function(req, res) {
+                var id = req.param('id');
+                var sql='select* from users';
+                if(id){
+                    sql += ' where id ='+id;
+                }
+                db.any(sql)
+                 .then(function(data){
+                     console.log('DATA:'+data);
+                     res.render('pages/users',{users: data})
+                     
+                 })
+                 .catch(function(error){
+                     console.log('ERROR:'+error);
+                 })
+     
+                 });
+
+  console.log('Appp is running at http://localhost:8080');          
+
+app.listen(8080);
