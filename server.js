b3be04f54d146d1d7ef589b61e447b675ef4adcf
Jirawat Paenkaew <jirawat_pao@hotmail.co.th>
2018-09-17 16:48:41 +0700

    add header

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
-var mysql = require('mysql')
+var app = express();
+var mysql = require('mysql');
+//app.use(express.static ('static') );
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+        
+    });
+
 var connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : 'www.db4free.net',
   user     : 's140390',
   password : 'abc123**',
   database : 'db140390'
@@ -12,62 +21,10 @@ connection.connect()
 connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
   if (err) throw err
 
-  console.log('The solution is: ', rows[0].solution)
+  console.log('The solution is: ', rows)
 })
 
-connection.end()
-//app.use(express.static ('static') );
-app.set('view engine', 'ejs');
+connection.end();
+console.log('Appp is running at http://localhost:8085');          
 
-app.get('/', function(req, res) {
-    res.render('pages/index');
-        
-    });
-
-    app.get('/about', function(req, res) {
-        var name = ['BUBBLE'];
-        var hobbies = ['Music','Movie','Programming'];
-        var bdate ='27/03/1997';
-        res.render('pages/about',{fullname : name, hobbies:hobbies,bdate: bdate});
-            
-        });
-    //Display all products
-        app.get('/products', function(req, res) {
-            var id = req.param('id');
-            var sql='select* from products';
-                if(id){
-                    sql += ' where id ='+id;
-                }
-           db.any(sql)
-            .then(function(data){
-                console.log('DATA:'+data);
-                res.render('pages/products',{products: data})
-                
-            })
-            .catch(function(error){
-                console.log('ERROR:'+error);
-            })
-
-        });
-   //Display all user
-            app.get('/users/:id', function(req, res) {
-                var id = req.param('id');
-                var sql='select* from users';
-                if(id){
-                    sql += ' where id ='+id;
-                }
-                db.any(sql)
-                 .then(function(data){
-                     console.log('DATA:'+data);
-                     res.render('pages/users',{users: data})
-                     
-                 })
-                 .catch(function(error){
-                     console.log('ERROR:'+error);
-                 })
-     
-                 });
-
-  console.log('Appp is running at http://localhost:8080');          
-
-app.listen(8080);
+app.listen(8085);
