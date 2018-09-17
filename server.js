99ae2dc663b232265233bb22da9947b1a29bc6de
Jirawat Paenkaew <jirawat_pao@hotmail.co.th>
2018-09-17 17:00:44 +0700

    changes directory

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var connection = mysql.createConnection({
 
 connection.connect()
 
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+connection.query('SELECT * from students', function (err, rows, fields) {
   if (err) throw err
 
   console.log('The solution is: ', rows)
